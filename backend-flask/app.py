5430f0e6208479ed8e0ce5205f9116094b4bb015
Jahina <jaxwork9@gmail.com>
2026-02-25 15:38:36 +0100

    Modification de app.py pour que ce soit le badge qui soit lu

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,38 +3,45 @@ from ldap3 import Server, Connection, ALL
 
 app = Flask(__name__)
 
+LDAP_SERVER = "ldap://localhost"
+BASE_DN = "dc=giga-factory,dc=local"
+
 @app.route("/")
 def index():
     return "Serveur Flask opérationnel!"
 
-@app.route("/auth_ldap", methods=["POST"])
-def auth_ldap():
+@app.route("/check_badge", methods=["POST"])
+def check_badge():
    # 1. Récupérer les données envoyées par le client
 
    data = request.get_json()
 
-   username = data.get("username")
-   password = data.get("password")
+   badgenumber = data.get("badgenumber")
 
    # 2. Vérifier que les champs existent
 
-   if not username or not password :
-      return jsonify({"error":"username et password requis"}), 400
+   if not badge :
+      return jsonify({"access":False, "message":"badge non lu"}), 400
 
    # 3. Configuration bonne route LDAP
 
-   LDAP_SERVER = "ldap://localhost:389"
-   BASE_DN = "dc=giga-factory,dc=local"
-   USER_DN = f"uid={username},ou=People,{BASE_DN}"
+   server = Server(LDAP_SERVER, get_info=ALL)
+   conn = Connection(server, auto_bind=True)
 
-   try:
-       server = Server(LDAP_SERVER, get_info=ALL)
-       conn = Connection(server, user=USER_DN, password=password, auto_bind=True)
+   search_base = f"ou=People,{BASE_DN}"
+   search_filter = f"(employeeNumber={badge})"
 
-       return jsonify({"status":"success", "message":"Authentification LDAP reussie"}), 200
+   conn.search(
+       search_base=search_base,
+       search_filtrer=search_filter,
+       attributes=["uid"]
+   )
 
-   except Exception:
-       return jsonify({"status":"fail", "message":"Echec authentification LDAP"}), 401
+   if conn.entries:
+       return jsonify({"access":"True"})
+
+   else:
+       return jsonify({"access":"False"})
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True)
+   app.run(host="0.0.0.0", port=5000)
